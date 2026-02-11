64b996086ca17dac9d9ed51176e11110403d84fa
Vinni <vinni@gmail.com>
2026-02-10 21:16:10 -0500

    Operaciones multiplicar

---
+++ src/util/Operaciones.java
@@ -0,0 +1,15 @@
+package util;
+
+public class Operaciones {
+    public long multiplicar(String a , Long b){
+        long numero = Long.parseLong(a);
+
+        return numero+b.longValue();
+    }
+    public long multiplicar(Long a , Long b){
+
+
+        return a.longValue()+b.longValue();
+    }
+
+}
