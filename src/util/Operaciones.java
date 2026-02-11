9a8481b0bd92b668c5cf5372332adb14cced4c20
Vinni <vinni@gmail.com>
2026-02-10 21:09:32 -0500

    Operacion sumar sobrecargada y verificada

---
+++ src/util/Operaciones.java
@@ -0,0 +1,15 @@
+package util;
+
+public class Operaciones {
+    public long sumar(String a , Long b){
+        long numero = Long.parseLong(a);
+
+        return numero+b.longValue();
+    }
+    public long sumar(Long a , Long b){
+
+
+        return a.longValue()+b.longValue();
+    }
+
+}
